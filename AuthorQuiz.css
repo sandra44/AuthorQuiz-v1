8e7b7b80be96e1bb8ae83790dacf919e3b68ec4c
Sandra Anil <sandraanil44@gmail.com>
2018-11-26 09:04:23 +0530

    Create AuthorQuiz.css

---
+++ AuthorQuiz.css
@@ -0,0 +1,63 @@
+.answer {
+	border-left: 4px solid LightBlue;
+	background-color: rgb(230, 248, 254);
+	border-bottom: 1px solid lightblue;
+	padding: 12px 10px;
+	margin-bottom: 15px;
+	margin-top: 15px;
+}
+.answer:hover{
+	background-color: LightBlue;
+}
+.authorimage {
+	border-top : 30px;
+	margin-top: 30px;
+	height: 250px;
+	max-width: 200px;
+}
+
+.options{
+	margin-top: 10px;
+	margin-bottom: 10px;
+}
+
+.score {
+	display: inline-block;
+	margin-left: 500px;
+}
+
+.score-badge{
+	display: inline-block;
+	padding: 10px;
+	margin-left: 5px;
+	color: #007bff;
+	border-radius: 20px;
+    background-color: #FFFFFF;
+}
+
+.addAuthor{
+	text-decoration: none;
+	color: #FFFFFF;
+}
+
+.addAuthor:hover{
+	text-decoration: none;
+	color: #FFFFFF;
+}
+
+.cntBtn {
+	float: right;
+}
+
+.footer {
+	margin-top: 20px;
+	margin-left: 500px;
+}
+.pass {
+	background-color: Green;
+	height: 250px;
+}
+.fail {
+	background-color: Red;
+	height: 250px;
+}
