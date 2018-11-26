685b3f39ef7f52203da3e61dc4f5651197db8b21
Sandra Anil <sandraanil44@gmail.com>
2018-11-26 09:34:36 +0530

    Update AuthorQuiz.tsx

--- AuthorQuiz.tsx
+++ AuthorQuiz.tsx
@@ -1,63 +1,24 @@
-.answer {
-	border-left: 4px solid LightBlue;
-	background-color: rgb(230, 248, 254);
-	border-bottom: 1px solid lightblue;
-	padding: 12px 10px;
-	margin-bottom: 15px;
-	margin-top: 15px;
-}
-.answer:hover{
-	background-color: LightBlue;
-}
-.authorimage {
-	border-top : 30px;
-	margin-top: 30px;
-	height: 250px;
-	max-width: 200px;
+import * as React from 'react';
+import { Link} from 'react-router-dom';
+import './assets/bootstrap.min.css';
+import './AuthorQuiz.css';
+import Hero from './Hero';
+import Turn from './Turn';
+
+class AuthorQuiz extends React.Component<any, any>{
+        constructor(props:any){
+        super(props);
+    }
+
+    render(){
+        return(
+            <div className="container-fluid">
+                <Hero />
+                <Turn turnData={this.props.turnData} nextQue={this.props.nextQue}/>
+                <button type="button" className="btn btn-primary"><Link className="addAuthor" to="/add">Add an author</Link></button>
+            </div>
+        );
+    }
 }
 
-.options{
-	margin-top: 10px;
-	margin-bottom: 10px;
-}
-
-.score {
-	display: inline-block;
-	margin-left: 500px;
-}
-
-.score-badge{
-	display: inline-block;
-	padding: 10px;
-	margin-left: 5px;
-	color: #007bff;
-	border-radius: 20px;
-    background-color: #FFFFFF;
-}
-
-.addAuthor{
-	text-decoration: none;
-	color: #FFFFFF;
-}
-
-.addAuthor:hover{
-	text-decoration: none;
-	color: #FFFFFF;
-}
-
-.cntBtn {
-	float: right;
-}
-
-.footer {
-	margin-top: 20px;
-	margin-left: 500px;
-}
-.pass {
-	background-color: Green;
-	height: 250px;
-}
-.fail {
-	background-color: Red;
-	height: 250px;
-}
+export default AuthorQuiz;
